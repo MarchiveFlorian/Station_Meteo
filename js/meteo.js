2e52def7456d344f8f2202ba85b8ba1aaaafa6f6
Florian Marchive <marchiveflorian@gmail.com>
2024-03-11 10:22:06 +0100

    setInterval 1 hour fait, 1ere version finie

--- js/meteo.js
+++ js/meteo.js
@@ -1,7 +1,14 @@
+//Ma clé API du site openweathermap
 const APIKEY = "a624fe8063bf9ec3dc002240c8174454";
 
+
+//Fonction pour effectuer la requête API
+function fetchWeatherData(){
+
+// Ici il faut changer Bordeaux par la ville souhaitée
 let url = `https://api.openweathermap.org/data/2.5/weather?q=Bordeaux&appid=${APIKEY}&units=metric&lang=fr`;
 
+//On récupère les valeurs de l'api au format json + ajout dans l'html
 fetch(url).then((response) => response.json().then((data) => {
      console.log(data);
      document.querySelector('#city').innerHTML = data.name;
@@ -9,3 +16,10 @@ fetch(url).then((response) => response.json().then((data) => {
     })
 )
 .catch((err) => console.log('Erreur : ' + err));
+}
+
+//1er appel de notre fonction au chargement de la page
+fetchWeatherData();
+
+//Nouvelle requête toutes les heures
+setInterval(fetchWeatherData, 3600000); // 3600000 ms = 1 heure
