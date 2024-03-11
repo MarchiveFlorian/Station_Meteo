0a05def377d148cf5a9192fe0dcd81be46a916f7
Florian Marchive <marchiveflorian@gmail.com>
2024-03-11 09:56:26 +0100

    fonctionnel, un peu de css (non responsive), manque le refresh toutes les 1h

--- js/meteo.js
+++ js/meteo.js
@@ -5,9 +5,7 @@ let url = `https://api.openweathermap.org/data/2.5/weather?q=Bordeaux&appid=${AP
 fetch(url).then((response) => response.json().then((data) => {
      console.log(data);
      document.querySelector('#city').innerHTML = data.name;
-     document.querySelector('#temp').innerHTML = data.main.temp + '°';
-     document.querySelector('#humidity').innerHTML = data.main.humidity + '%';
-     document.querySelector('#wind').innerHTML = data.wnd.speed + 'km/h';
-     
+     document.querySelector('#temp').innerHTML = data.main.temp + '°';  
     })
-);
+)
+.catch((err) => console.log('Erreur : ' + err));
