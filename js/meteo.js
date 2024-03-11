6bb5465372ca21cbf30804deb5a9319a1694edf2
Florian Marchive <marchiveflorian@gmail.com>
2024-03-11 09:08:59 +0100

    récupération des données de l'api + transformer en json dans la console

---
+++ js/meteo.js
@@ -0,0 +1,4 @@
+const APIKEY = "a624fe8063bf9ec3dc002240c8174454";
+
+let url = `https://api.openweathermap.org/data/2.5/weather?q=Bordeaux&appid=${APIKEY}&units=metric&lang=fr`;
+fetch(url).then((response) => response.json().then((data) => console.log(data)));
